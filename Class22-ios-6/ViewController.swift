f87013c2f71aca54227efc0e57155d38f8e02efc
guymacin <guyskv@gmail.com>
2022-05-02 17:54:19 +0300

    Initial Commit

---
+++ Class22-ios-6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Class22-ios-6
+//
+//  Created by Guy Macin on 02/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
