fcbf1c0b511202d16e08f3ef753b667176f64eee
guymacin <guyskv@gmail.com>
2022-05-09 18:10:36 +0300

    inner delegate

--- Class22-ios-6/CarCell.swift
+++ Class22-ios-6/CarCell.swift
@@ -1,10 +1,22 @@
 import Foundation
 import UIKit
 
+protocol CallBack_CarCell {
+    func carClicked(position: Int)
+}
+
 class CarCell: UITableViewCell {
     @IBOutlet weak var car_LBL_name: UILabel!
     @IBOutlet weak var car_LBL_desc: UILabel!
-    @IBOutlet weak var car_IMG_heart: UIImageView!
+    @IBOutlet weak var car_BTN_heart: UIButton!
     
+    
+    var delegate: CallBack_CarCell?
+    var position: Int?
+    
+    @IBAction func likedClicked(_ sender: Any) {
+        print("likedClicked")
+        delegate?.carClicked(position: position ?? 0)
+    }
     
 }
--- Class22-ios-6/ViewController.swift
+++ Class22-ios-6/ViewController.swift
@@ -15,17 +15,29 @@ class ViewController: UIViewController {
         main_LST_cars.delegate = self
         main_LST_cars.dataSource = self
         
-
+        
     }
 
 
+}
+
+extension ViewController: CallBack_CarCell {
+    
+    func carClicked(position: Int) {
+        if let b = cars?[position].liked {
+            cars?[position].liked = !b
+            main_LST_cars.reloadData()
+            print("ViewController - carClicked \(position)")
+        }
+
+    }
+    
 }
 
 extension ViewController:  UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if let car = cars?[indexPath.row] {
             print(car.name!)
-        
         }
     }
 }
@@ -39,16 +51,20 @@ extension ViewController:  UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         var cell: CarCell? = main_LST_cars.dequeueReusableCell(withIdentifier: "car_cell") as! CarCell
-        
+        print("tableView \(indexPath.row)")
+
         
         if let car = cars?[indexPath.row] {
+            cell?.delegate = self
+            cell?.position = indexPath.row
+            
             cell?.car_LBL_name.text = car.name
             cell?.car_LBL_desc.text = "\(car.price!)₪ | \(car.rate!) km/L"
             
             if (car.liked) {
-                cell?.car_IMG_heart.image = #imageLiteral(resourceName: "heart_filled")
+                cell?.car_BTN_heart.imageView?.image = #imageLiteral(resourceName: "heart_filled")
             } else {
-                cell?.car_IMG_heart.image = #imageLiteral(resourceName: "heart_linear")
+                cell?.car_BTN_heart.imageView?.image = #imageLiteral(resourceName: "heart_linear")
             }
         }
         
